d0b0f4279720b481ddb204d14653e51004832ecf
JoKeeR <33557806+JoKeRxTD@users.noreply.github.com>
2021-12-09 17:23:35 +0000

    more updates

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,44 +1,44 @@
-document.onkeydown = function (e) {
-  if (event.keyCode == 123) {
-    console.log("Looks like someone is trying to access something!");
+// document.onkeydown = function (e) {
+//   if (event.keyCode == 123) {
+//     console.log("Looks like someone is trying to access something!");
 
-    alert("Hmm.... What's going on here? 🤔");
+//     alert("Hmm.... What's going on here? 🤔");
 
-    return false;
-  }
+//     return false;
+//   }
 
-  if (e.ctrlKey && e.shiftKey && e.keyCode == "I".charCodeAt(0)) {
-    console.log("Looks like someone is trying to access something!");
+//   if (e.ctrlKey && e.shiftKey && e.keyCode == "I".charCodeAt(0)) {
+//     console.log("Looks like someone is trying to access something!");
 
-    alert("Hmm.... What's going on here? 🤔");
+//     alert("Hmm.... What's going on here? 🤔");
 
-    return false;
-  }
+//     return false;
+//   }
 
-  if (e.ctrlKey && e.shiftKey && e.keyCode == "C".charCodeAt(0)) {
-    console.log("Looks like someone is trying to access something!");
+//   if (e.ctrlKey && e.shiftKey && e.keyCode == "C".charCodeAt(0)) {
+//     console.log("Looks like someone is trying to access something!");
 
-    alert("Hmm.... What's going on here? 🤔");
+//     alert("Hmm.... What's going on here? 🤔");
 
-    return false;
-  }
+//     return false;
+//   }
 
-  if (e.ctrlKey && e.shiftKey && e.keyCode == "J".charCodeAt(0)) {
-    console.log("Looks like someone is trying to access something!");
+//   if (e.ctrlKey && e.shiftKey && e.keyCode == "J".charCodeAt(0)) {
+//     console.log("Looks like someone is trying to access something!");
 
-    alert("Hmm.... What's going on here? 🤔");
+//     alert("Hmm.... What's going on here? 🤔");
 
-    return false;
-  }
+//     return false;
+//   }
 
-  if (e.ctrlKey && e.keyCode == "U".charCodeAt(0)) {
-    console.log("Looks like someone is trying to access something!");
+//   if (e.ctrlKey && e.keyCode == "U".charCodeAt(0)) {
+//     console.log("Looks like someone is trying to access something!");
 
-    alert("Hmm.... What's going on here? 🤔");
+//     alert("Hmm.... What's going on here? 🤔");
 
-    return false;
-  }
-};
+//     return false;
+//   }
+// };
 
 document.addEventListener("contextmenu", (e) => e.preventDefault());
 
